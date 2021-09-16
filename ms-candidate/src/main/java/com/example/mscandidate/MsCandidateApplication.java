93264dc1dacbb5b1f88e0c16f609029e6d904144
Miqueias Sousa <mi.queias2010@hotmail.com>
2021-09-16 01:21:44 -0300

    project ms-candidate recreated

---
+++ ms-candidate/src/main/java/com/example/mscandidate/MsCandidateApplication.java
@@ -0,0 +1,13 @@
+package com.example.mscandidate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsCandidateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsCandidateApplication.class, args);
+	}
+
+}
---
+++ ms-candidate/src/test/java/com/example/mscandidate/MsCandidateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mscandidate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsCandidateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
